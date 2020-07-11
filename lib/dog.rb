b4c8e27c5eed1c0483d3511be2fa878b65bd0cee
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-11 01:45:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,28 @@ class Dog
   
   def inititalize(name)
     @name = name 
-    @@all << self
+    self.save
   end
   
   def self.all
   @@all
-end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def name 
+    @name
+  end
 end
   
